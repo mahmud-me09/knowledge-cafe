039b59c5aab426600b1708a7bd227445d329e2c5
mahmud-me09 <mahmud.me09@gmail.com>
2024-03-14 19:49:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-red-300 text-center text-3xl font-extrabold py-5'>Knowledge cafe</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Blogs/Blog.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const Blogs = () =>{
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(()=>{
+        
+    },[])
+}
